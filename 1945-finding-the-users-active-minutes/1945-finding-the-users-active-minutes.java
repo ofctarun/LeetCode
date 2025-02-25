8c4058b8d017e5af7719f1e3d5f275b8bed3995f
Tarun Narayanashetti <150335429+ofctarun@users.noreply.github.com>
2025-02-24 19:36:49 -0800

    Time: 14 ms (98.65%), Space: 56.7 MB (53.72%) - LeetHub

--- 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.java
+++ 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.java
@@ -1,12 +1,18 @@
 class Solution {
     public int[] findingUsersActiveMinutes(int[][] logs, int k) {
         HashMap<Integer,Set<Integer>> hm = new HashMap<>();
-        for(int[] log : logs){
-            int user = log[0];
-            int min = log[1];
-            hm.putIfAbsent(user,new HashSet<>());
-            hm.get(user).add(min);
+        for(int i=0;i<logs.length;i++) {
+            var cur = hm.get(logs[i][0]);
+            if (cur!=null) {
+                cur.add(logs[i][1]);
+            }
+            else{
+                var newH = new HashSet();
+                newH.add(logs[i][1]);
+                hm.put(logs[i][0],newH);
+            }
         }
+        int[] res = new int[k];
         int ans[] = new int[k];
         for(Set<Integer> min : hm.values()){
             ans[min.size()-1]++;
