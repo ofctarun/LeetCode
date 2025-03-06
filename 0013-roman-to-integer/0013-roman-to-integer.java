2e26bff1f58f2a7a766399353d7d9d9b122f4175
Tarun Narayanashetti <150335429+ofctarun@users.noreply.github.com>
2025-03-06 21:01:58 +0530

    Time: 2 ms (100%), Space: 45.1 MB (24.05%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,46 +1,32 @@
 class Solution {
     public int romanToInt(String s) {
-        int ans = 0,idx=0;
-        while(idx<s.length()){
-            if(s.charAt(idx)=='I'){
-                if((idx+1)<s.length() && s.charAt(idx+1)=='V'){
-                    ans+=4;
-                    idx++;
-                }
-                else if((idx+1)<s.length() && s.charAt(idx+1)=='X'){
-                    ans+=9;
-                    idx++;
-                }
-                else ans+=1;
-            }
-            else if(s.charAt(idx)=='V')ans+=5;
-            else if(s.charAt(idx)=='X'){
-                if((idx+1)<s.length() && s.charAt(idx+1)=='L'){
-                    ans+=40;
-                    idx++;
-                }
-                else if((idx+1)<s.length() && s.charAt(idx+1)=='C'){
-                    ans+=90;
-                    idx++;
-                }
-                else ans+=10;
-            }
-            else if(s.charAt(idx)=='L')ans+=50;
-            else if(s.charAt(idx)=='C'){
-                if((idx+1)<s.length() && s.charAt(idx+1)=='D'){
-                    idx++;
-                    ans+=400;
-                }
-                else if((idx+1)<s.length() && s.charAt(idx+1)=='M'){
-                    ans+=900;
-                    idx++;
-                }
-                else ans+=100;
-            }
-            else if(s.charAt(idx)=='D')ans+=500;
-            else ans+=1000;
-            idx++;
+        int res = 0;
+	    for (int i=s.length()-1;i>=0;i--){
+		    char c = s.charAt(i);
+		    switch(c){
+		        case 'I':
+			        res+=(res >= 5 ? -1 : 1);
+			        break;
+		        case 'V':
+			        res+=5;
+			        break;
+		        case 'X':
+			        res+=10*(res >= 50 ? -1 : 1);
+			        break;
+		        case 'L':
+			        res+=50;
+			        break;
+                case 'C':
+                    res+=100*(res >= 500 ? -1 : 1);
+                    break;
+                case 'D':
+                    res+=500;
+                    break;
+                case 'M':
+                    res+=1000;
+			    break;
+		}
         }
-        return ans;
+        return res;
     }
 }
