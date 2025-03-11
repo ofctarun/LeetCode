b3b1833cbeb14cea4c1a4ebdede8992b8b0774d2
Tarun Narayanashetti <150335429+ofctarun@users.noreply.github.com>
2025-03-11 23:42:07 +0530

    Time: 11 ms (91.15%), Space: 44.9 MB (37.47%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<s.length();i++){
             idx[s.charAt(i)-'a'] = i;
             if(idx[0]!=-1 && idx[1]!=-1 && idx[2]!=-1){
-                ans += (1 + Math.min(Math.min(idx[0],idx[1]),idx[2]));
+                ans += (1 + Math.min(idx[0],Math.min(idx[1],idx[2])));
             }
         }
         return ans;
