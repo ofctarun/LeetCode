7fda75bf25db6f5c08a529d2e35b9434e67a4e38
Tarun Narayanashetti <150335429+ofctarun@users.noreply.github.com>
2025-07-14 23:27:18 +0530

    Time: 7 ms (93.15%), Space: 62.6 MB (83.9%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
@@ -5,14 +5,11 @@ class Solution {
             deg[road[0]]++;
             deg[road[1]]++;
         }
-        Integer nodes[] = new Integer[n];
-        for(int i=0;i<n;i++)nodes[i] = i;
-        Arrays.sort(nodes,(a,b) -> deg[b] - deg[a]);
-        int val[] = new int[n];
-        int imp = n;
-        for(int node : nodes)val[node] = imp--;
+        Arrays.sort(deg);
         long ans = 0;
-        for(int[] road : roads)ans += val[road[0]] + val[road[1]];
+        for(int i=0;i<n;i++){
+            ans += (long)deg[i] * (i+1);
+        }
         return ans;
     }
 }
