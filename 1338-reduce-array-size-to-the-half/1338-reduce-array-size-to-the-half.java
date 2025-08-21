eeb962a44deba975f003352850c7682684b25708
Tarun Narayanashetti <150335429+ofctarun@users.noreply.github.com>
2025-08-22 00:07:09 +0530

    Time: 33 ms (82.04%), Space: 62 MB (39.48%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,13 +1,15 @@
 class Solution {
     public int minSetSize(int[] arr) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         HashMap<Integer,Integer> hm = new HashMap<>();
-        for(var el : arr)hm.put(el,hm.getOrDefault(el,0)+1);
-        for(var el : hm.values())pq.offer(el);
-        int c=0,ans=0;
-        while(c<arr.length/2){
-            c+=pq.poll();
+        for(var el : arr) hm.put(el, hm.getOrDefault(el, 0) + 1);
+        List<Integer> freq = new ArrayList<>(hm.values());
+        Collections.sort(freq); 
+        int c = 0, ans = 0;
+        for (int i=freq.size()-1;i>=0;i--) {
+            c += freq.get(i);
             ans++;
+            if (c >= arr.length / 2) break;
         }
         return ans;
     }
