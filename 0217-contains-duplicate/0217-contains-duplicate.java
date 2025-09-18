3b24882a0d77dc4a789516babd8deba271a13146
Tarun Narayanashetti <150335429+ofctarun@users.noreply.github.com>
2025-09-18 17:57:23 +0530

    Time: 19 ms (37.39%), Space: 59.6 MB (22.42%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> hs = new HashSet<>();
-        for(var num : nums){
+        for(int num : nums){
             hs.add(num);
         }
-        return nums.length != hs.size();
+        return !(hs.size() == nums.length);
     }
 }
