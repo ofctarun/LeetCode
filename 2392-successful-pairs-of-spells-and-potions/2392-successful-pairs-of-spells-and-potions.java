747802716dcbb8a4b4d3973554788d79d18530cb
Tarun Narayanashetti <150335429+ofctarun@users.noreply.github.com>
2025-10-09 05:13:52 +0530

    Time: 46 ms (69.58%), Space: 65 MB (55.83%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
-        Arrays.sort(potions);
         int n = spells.length;
-        int ans[] = new int[n];
-        for(int i = 0; i < n; i++){
-            int idx = bs(potions, spells[i], success);
-            if(idx != -1)ans[i] = potions.length - idx;
+        int m = potions.length;
+        int[] ans = new int[n];
+        Arrays.sort(potions);
+        for(int k = 0; k < n; k++){
+            int i = 0,j = m - 1;
+            while(i<=j){
+                int mid = i + ((j - i) / 2);
+                long pair = (long) spells[k] * potions[mid];
+                if(pair >= success){
+                    j = mid - 1;
+                }else{
+                    i = mid + 1;
+                }
+            }
+            ans[k] = m - i;
         }
         return ans;
-    }
-    int bs(int potions[], long strength, long success) {
-        int low = 0,high = potions.length - 1,idx = -1;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if((long) potions[mid] * strength >= success){
-                idx = mid;
-                high = mid - 1;
-            }else{
-                low = mid + 1;
-            }
-        }
-        return idx;
     }
 }
