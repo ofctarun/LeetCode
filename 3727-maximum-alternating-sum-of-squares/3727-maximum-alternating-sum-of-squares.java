cf46ce9bed88ca220072f5d7b99c48d5f8025fb2
Tarun Narayanashetti <150335429+ofctarun@users.noreply.github.com>
2025-11-02 01:06:07 +0530

    Time: 30 ms (64.1%), Space: 125.6 MB (5.6%) - LeetHub

--- 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.java
+++ 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.java
@@ -5,15 +5,15 @@ class Solution {
         }
         Arrays.sort(arr);
         int idx1=0, idx2=arr.length/2;
-        long sum = 0;
+        long altScore = 0;
         while(idx1<arr.length/2){
-            sum += arr[idx2]-arr[idx1];
+            altScore += arr[idx2]-arr[idx1];
             idx2++;
             idx1++;
         }
         if(arr.length%2!=0){
-            sum +=arr[idx2];
+            altScore +=arr[idx2];
         }
-        return sum;
+        return altScore;
     }
 }
