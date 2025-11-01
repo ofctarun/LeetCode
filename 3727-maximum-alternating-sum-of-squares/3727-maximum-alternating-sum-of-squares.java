dc3342210d4550f4a3765e4375008a04328cfe60
Tarun Narayanashetti <150335429+ofctarun@users.noreply.github.com>
2025-11-02 00:54:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.java
+++ 3727-maximum-alternating-sum-of-squares/3727-maximum-alternating-sum-of-squares.java
@@ -1,13 +1,17 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            nums[i] = Math.abs(nums[i]);
+        for(int i = 0; i < n; i++){
+            if(nums[i] < 0)nums[i] *= -1;
         }
         Arrays.sort(nums);
         long ans = 0;
-        for(int i=0;i<nums.length;i++){
-            if(i<nums.length/2)ans -= nums[i]*nums[i];
-            else ans += nums[i]*nums[i];
+        for(int i=0;i<nums.length/2;i++){
+            long val = nums[i] * nums[i];
+            ans -= val;
+        }
+        for(int i=nums.length/2;i<nums.length;i++){
+            long val = nums[i]*nums[i];
+            ans += val;
         }
         return ans;
     }
