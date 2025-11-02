ab955187824a832db1aee7d7daf0d5636b3dd089
Tarun Narayanashetti <150335429+ofctarun@users.noreply.github.com>
2025-11-03 00:41:33 +0530

    Time: 24 ms (62.77%), Space: 147.3 MB (5.11%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.java
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.java
@@ -1,29 +1,27 @@
-class Solution {
-    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] grid = new int[m][n];
-        for (int[] w : walls) grid[w[0]][w[1]] = -1;
-        for (int[] g : guards) grid[g[0]][g[1]] = 1;
-        int[] d = {0, 1, 0, -1, 0};
-        for(int[] g : guards){
-            int x = g[0], y = g[1];
-            for(int k = 0; k < 4; k++){
-                int i = x + d[k], j = y + d[k + 1];
-                while (i >= 0 && i < m && j >= 0 && j < n) {
-                    int cell = grid[i][j];
-                    if (cell == -1 || cell == 1) break;
-                    if (cell == 0) grid[i][j] = 2;
-                    i += d[k];
-                    j += d[k + 1];
+class Solution{
+    public int countUnguarded(int r,int c,int[][] G,int[][] W){
+        int[][] A=new int[r][c];
+        for(int[] p:W)A[p[0]][p[1]]=-1;
+        for(int[] p:G)A[p[0]][p[1]]=1;
+        int[] D={0,1,0,-1,0};
+        for(int[] p:G){
+            int x=p[0],y=p[1];
+            for(int d=0;d<4;d++){
+                int i=x+D[d],j=y+D[d+1];
+                while(i>=0&&i<r&&j>=0&&j<c){
+                    int v=A[i][j];
+                    if(v==-1||v==1)break;
+                    if(v==0)A[i][j]=2;
+                    i+=D[d];
+                    j+=D[d+1];
                 }
             }
         }
-        int count = 0;
-        for (int i = 0; i < m; i++) {
-            int[] row = grid[i];
-            for (int j = 0; j < n; j++)
-                if (row[j] == 0) count++;
+        int z=0;
+        for(int i=0;i<r;i++){
+            int[] Q=A[i];
+            for(int j=0;j<c;j++)if(Q[j]==0)z++;
         }
-
-        return count;
+        return z;
     }
 }
