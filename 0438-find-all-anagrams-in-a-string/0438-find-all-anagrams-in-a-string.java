674773b7f68dbc4cd4e2fc9d04e5f896e681e9ac
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-03-11 16:03:28 +0530

    Time: 18 ms (39.68%), Space: 46.7 MB (90.08%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -4,22 +4,21 @@ class Solution {
         int freq[] = new int[26];
         List<Integer> ans = new ArrayList<>();
         for(char ch : p.toCharArray()){
-            if(freq[ch - 'a'] == 0)unq++;
-            freq[ch - 'a']--;
+            int c = ch - 'a';
+            if(freq[c] == 0)unq++;
+            freq[c]--;
         }
         int i = 0,j = 0;
         while(j < s.length()){
-            int curr = s.charAt(j) - 'a';
-            if(freq[curr] == 0)unq++;
-            freq[curr]++;
-            if(freq[curr] == 0)unq--;
+            if(freq[s.charAt(j) - 'a'] == 0)unq++;
+            freq[s.charAt(j) - 'a']++;
+            if(freq[s.charAt(j) - 'a'] == 0)unq--;
             if(j - i + 1 < p.length())j++;
             else{
                 if(unq == 0)ans.add(i);
-                int cur = s.charAt(i) - 'a';
-                if(freq[cur] == 0)unq++;
-                freq[cur]--;
-                if(freq[cur] == 0)unq--;
+                if(freq[s.charAt(i) - 'a'] == 0)unq++;
+                freq[s.charAt(i) - 'a']--;
+                if(freq[s.charAt(i) - 'a'] == 0)unq--;
                 i++;
                 j++;
             }
