6f48633be55c06d130b2f547339122aeb5076bec
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-06-09 22:21:07 +0530

    Time: 992 ms (54.06%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,4 +1,7 @@
-select e.name as department,d.name as employee , salary from 
-(select name,departmentId,salary,dense_rank() over (partition by departmentId order by salary desc) as rk from employee) e
-left join department d on e.departmentId = d.id
-where rk <= 3
+# Write your MySQL query statement below
+with cte1 as (select name, salary, dense_rank()over(partition by departmentId order by salary desc)as ranking, departmentId 
+from employee)
+select b. name as department, a.name as employee, a.salary
+from cte1 a inner join department b
+on a.departmentId = b.id
+where a.ranking <= 3;
