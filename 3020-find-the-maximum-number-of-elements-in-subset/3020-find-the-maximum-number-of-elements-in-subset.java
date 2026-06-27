34a15f9242d25ba134d951c8678c2e88c201c043
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-06-27 10:37:43 +0530

    Time: 53 ms (81.4%), Space: 67.1 MB (60.46%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.java
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.java
@@ -2,12 +2,11 @@ class Solution {
     public int maximumLength(int[] nums) {
         HashMap<Integer, Integer> hm = new HashMap<>();
         int ans = 1;
-        for(int num : nums)hm.put(num, hm.getOrDefault(num, 0) + 1);
-        if(hm.containsKey(1)){
-            ans = hm.get(1);
-            ans = (ans % 2 == 0) ? ans - 1 : ans;
-            hm.remove(1);
+        for(int num : nums){
+            if(num == 1)ans++;
+            else hm.put(num, hm.getOrDefault(num, 0) + 1);
         }
+        if(ans-- > 0)ans = (ans % 2 == 0) ? ans - 1 : ans;
         for(int num : hm.keySet()){
             int len = 0;
             while(hm.containsKey(num)){
