fff6b4fffb1d8b2a329cbee7175af6740547d828
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-07-27 19:06:03 +0530

    Time: 2 ms (99.88%), Space: 46.1 MB (28.16%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int[] count = new int[128];
+        for(char c : moves.toCharArray())count[c]++;
+        return count['U'] == count['D'] && count['R'] == count['L'];
+
+    }
+}
