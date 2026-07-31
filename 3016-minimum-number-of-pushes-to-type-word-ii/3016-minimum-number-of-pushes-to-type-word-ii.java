bd235d31a2ad2c686b5717f22677bc36f6564238
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-07-31 12:34:44 +0530

    Time: 10 ms (66.76%), Space: 47.9 MB (73.7%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -3,15 +3,8 @@ class Solution {
         int[] freq = new int[26];
         for(char ch : word.toCharArray())freq[ch - 'a']++;
         Arrays.sort(freq);
-        int c = 0,ans = 0;
-        for(int i = 25 ; i >= 0; i--){
-            if(freq[i] != 0)c++;
-            else break;
-            if(c <=  8) ans += freq[i];
-            else if (c <= 16)ans += 2 * freq[i];
-            else if (c <= 24) ans += 3 * freq[i];
-            else ans += 4 * freq[i];
-        }
+        int ans = 0;
+        for(int i=25;i>=0;i--)ans+=freq[i]*((25-i)/8+1);
         return ans;
     }
 }
