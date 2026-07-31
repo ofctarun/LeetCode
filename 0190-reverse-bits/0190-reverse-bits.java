e08958fed37afd557391d94689111a5cf64cf1f8
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-07-31 21:27:47 +0530

    Time: 0 ms (100%), Space: 42.4 MB (53.08%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,6 +1,10 @@
-public class Solution {
-    // you need treat n as an unsigned value
+class Solution {
     public int reverseBits(int n) {
-        return Integer.reverse(n);
+        int ans = 0;
+        for(int i = 0; i < 32; i++){
+            ans = (ans << 1 ) | (n & 1);
+            n >>= 1;
+        }
+        return ans;
     }
 }
