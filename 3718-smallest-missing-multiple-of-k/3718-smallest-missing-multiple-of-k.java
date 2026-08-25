1485fc0ef0e1102f976862ab156b04628b987309
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-08-26 01:02:45 +0530

    Time: 93 ms (5.78%), Space: 45.1 MB (66.75%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,14 +1,13 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        int id[] = new int[101];
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%k==0){
-                id[nums[i]/k]++;
+        int n = nums.length;
+        int a = k;
+        for(int i=0;i<n;i++){
+            if(nums[i] == a){
+                a = a + k;
+                i = -1;
             }
         }
-        for(int i = 1;i < 100; i++){
-            if(id[i] == 0)return i*k;
-        }
-        return 101*k;
+        return a;
     }
 }
