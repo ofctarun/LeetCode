bfc31d6f5862f9a49080adae29575f408b2c122a
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-09-24 11:39:45 +0530

    Time: 1 ms (99.69%), Space: 45.5 MB (77.88%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -2,7 +2,7 @@ class Solution {
     public int smallestIndex(int[] nums) {
         if(nums[0] == 0)return 0;
         if(nums.length > 1 && nums[1] == 1000) return 1;
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 1; i < nums.length; i++){
             int dig = (int)Math.log10(nums[i]);
             if((dig == 0 && i == nums[i]) || (dig == 1 && ((nums[i]%10 + nums[i]/10) == i)) || (dig == 2 && ((nums[i]%10 + (nums[i]/10)%10 + nums[i]/100) == i)))return i;
         }
