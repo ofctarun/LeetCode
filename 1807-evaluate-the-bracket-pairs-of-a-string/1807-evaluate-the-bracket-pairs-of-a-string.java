674851e6fc40f590c564a339bd143dd2556251dd
Tarun Narayanashetti <narayanashettitarun01@gmail.com>
2026-09-26 10:17:32 +0530

    Time: 154 ms (6.33%), Space: 101.6 MB (5.43%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -5,9 +5,9 @@ class Solution {
             hm.put(ls.get(0),ls.get(1));
         }
         int i = 0;
-        StringBuilder sb = new StringBuilder();
+        String sb = "";
         while(i < s.length()){
-            if(s.charAt(i) != '(' && s.charAt(i) != ')')sb.append(s.charAt(i));
+            if(s.charAt(i) != '(' && s.charAt(i) != ')')sb+=(s.charAt(i));
             else{
                 i++;
                 String str = "";
@@ -15,11 +15,11 @@ class Solution {
                     str += s.charAt(i);
                     i++;
                 }
-                if(!hm.containsKey(str))sb.append('?');
-                else sb.append(hm.get(str));
+                if(!hm.containsKey(str))sb+=('?');
+                else sb+=(hm.get(str));
             }
             i++;
         }
-        return new String(sb);
+        return sb;
     }
 }
